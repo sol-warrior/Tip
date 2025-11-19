c4556949b467765f78ec2d95cd55b6670cc54f15
solwarrior <118261237+sol-warrior@users.noreply.github.com>
2025-11-19 15:15:57 +0530

    Add layout component for the builder, integrating Navbar and providing a structured layout for child components in the frontend.

---
+++ frontend/src/app/builder/layout.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/src/components/header/Navbar";
+import React, { ReactNode } from "react";
+
+const layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className=" h-screen bg-zinc-50 font-sans py-6 px-12">
+      <Navbar />
+      {children}
+    </div>
+  );
+};
+
+export default layout;
