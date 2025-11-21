02ec77260255d656499cef1231697afde68b14bf
solwarrior <118261237+sol-warrior@users.noreply.github.com>
2025-11-22 05:18:45 +0530

    Add formatAddress utility function to format strings for better readability, enhancing utility functions in the frontend.

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,16 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatAddress(text: string) {
+  const pre = text.slice(0, 5);
+  const last = text.slice(-5);
+
+  const format = `${pre}...${last}`;
+
+  console.log("Format : ", format);
+  return format;
 }
